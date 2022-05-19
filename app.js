627f7f2795e1f571507b4b3b82c61bf8d4b334aa
kukukaia <kukukaia@protonmail.com>
2022-05-19 01:24:57 -0300

    adicionando elementos

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 //criar uma constante
 const http = require('http');
+//const utf8 = require('utf8');
 
-console.log('Iniciando o serviço na porta 9600');
+console.log('Iniciando o serviço na porta 9200');
 
 // funcao incluida no modulo http
 http.createServer(function (req,res) {
-    res.write('<h1>Olá mundo!!!</h1>'); //resposta enviada ao cliente
-    res.write('<h1>Olá mundo!!!</h1>'); //resposta enviada ao cliente
-    res.write('<h1>Olá mundo!!!</h1>'); //resposta enviada ao cliente
-    res.write('<h1>Olá mundo!!!</h1>'); //resposta enviada ao cliente
+    //res.write('<h1>'+utf8.encode('Olá mundo!!!</h1>')); //resposta enviada ao cliente
+    res.write('<h1>Ola mundo!!!</h1>'); //resposta enviada ao cliente
+    //res.write(req.url);  
     ///
     ///
     ///
     res.end();
-}).listen(9600);
+}).listen(9200);
