bf792f6f84bfeb2aeaa0543a68d89dec3ae5be23
kukukaia <kukukaia@protonmail.com>
2022-05-19 01:33:23 -0300

    Atualizando

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ console.log('Iniciando o serviço na porta 9200');
 http.createServer(function (req,res) {
     //res.write('<h1>'+utf8.encode('Olá mundo!!!</h1>')); //resposta enviada ao cliente
     res.write('<h1>Ola mundo!!!</h1>'); //resposta enviada ao cliente
-    //res.write(req.url);  
-    ///
-    ///
+    res.write(req.url);  
+    if(req.url='/10'){
+        res.write('<b>Você digitou o número 10 na requisição<b>');
+    }
     ///
     res.end();
 }).listen(9200);
