55045557b7451f47482e905633a898398facca93
kukukaia <kukukaia@protonmail.com>
2022-05-19 01:10:33 -0300

    Primeiro arquivo

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 //criar uma constante
 const http = require('http');
 
+console.log('Iniciando o serviço na porta 9600');
+
 // funcao incluida no modulo http
-http.createServer(function (req,res))
+http.createServer(function (req,res) {
+    res.write('<h1>Olá mundo!!!</h1>'); //resposta enviada ao cliente
+    res.write('<h1>Olá mundo!!!</h1>'); //resposta enviada ao cliente
+    res.write('<h1>Olá mundo!!!</h1>'); //resposta enviada ao cliente
+    res.write('<h1>Olá mundo!!!</h1>'); //resposta enviada ao cliente
+    ///
+    ///
+    ///
+    res.end();
+}).listen(9600);
