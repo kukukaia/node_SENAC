163b75c530e747f7778f5f671705f76c66e20152
kukukaia <kukukaia@protonmail.com>
2022-05-24 12:13:30 -0300

    incluindo arquivo app

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-//criar uma constante
-const http = require('http');
-//const fs = require('fs');//webserver
-//const path = require('path');
-//const utf8 = require('utf8');
+const express = require('express')
+const app = express()
 
-//console.log('Iniciando o serviço na porta 9200');
+const port=3000
 
-// funcao incluida no modulo http
-http.createServer(function (req,res) {
-    //res.write('<h1>'+utf8.encode('Olá mundo!!!</h1>')); //resposta enviada ao cliente
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Ola mundo!!!</h1>'); //resposta enviada ao cliente
-    //res.write(req.url);  
-    //if(req.url='/10'){
-        //res.write('<b>Você digitou o número 10 na requisição<b>');
-    //}
-    res.write(req.url);
-    res.end();//ifnaliza a resposta
-}).listen(9600);//inicializa o serviço na porta 9600
+app.get('/',(req,res)=>{
+    res.end("Webserver Ativo")
+})
+
+app.listen(port,console.log("Servidor ativo na porta 3000"))
